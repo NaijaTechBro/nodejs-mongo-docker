a935db7f33fda5cf02446ed0472d44c45b5b104b
NaijaTechBro <bakisodiq@gmail.com>
2023-05-10 12:23:08 +0100

    server

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
-require('dotenv').config();
-
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
-// security
-const cors = require('cors');
+app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: false }));
 
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser');
+// Connect to MongoDB
+mongoose
+  .connect(
+    'mongodb://mongo:27017/docker-node-mongo',
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
+const Item = require('./models/Item');
 
+app.get('/', (req, res) => {
+  Item.find()
+    .then(items => res.render('index', { items }))
+    .catch(err => res.status(404).json({ msg: 'No items found' }));
+});
 
-app.use(cors());
-app.use(bodyParser)
+app.post('/item/add', (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+
+  newItem.save().then(item => res.redirect('/'));
+});
+
+const port = 3000;
+
+app.listen(port, () => console.log('Server running...'));
